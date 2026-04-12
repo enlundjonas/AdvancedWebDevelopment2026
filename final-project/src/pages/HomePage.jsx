009142b6bd61ca7789d8b824fb6f25d878093ad0
Jonas Enlund <jonas.enlund@centria.fi>
2026-04-12 17:03:45 +0300

    Update HomePage.jsx

--- final-project/src/pages/HomePage.jsx
+++ final-project/src/pages/HomePage.jsx
@@ -1,1 +1,16 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+import Hero from '../components/hero'
+import Home from '../components/home'
 
+function HomePage() {
+  return (
+    <div className="bg-gray-100 text-gray-800 min-h-screen">
+      <Hero />
+      <Home />
+    </div>
+  );
+}
+
+
+export default HomePage;
