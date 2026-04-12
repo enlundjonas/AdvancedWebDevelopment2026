fdfa0db777962c9e56690d63e825d2ab13ac0186
Jonas Enlund <jonas.enlund@centria.fi>
2026-04-12 16:44:01 +0300

    Update App.jsx

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,18 +1,25 @@
-import Header from './components/header'
-import Footer from './components/footer'
-import Hero from './components/hero'
-import Home from './components/home'
+import Header from './components/header';
+import Footer from "./components/footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import OrdersPage from "./pages/OrderPage";
 
 function App() {
   return (
-    <div className="bg-gray-100 text-gray-800 min-h-screen">
-      <Header />
-      <Hero />
-      <Home />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="bg-gray-100 text-gray-800 min-h-screen">
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
-
 
 export default App;
