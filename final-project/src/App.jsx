35e6ead09e9fbf93e365c819be72f24f4e071837
Jonas Enlund <jonas.enlund@centria.fi>
2026-04-18 23:48:32 +0300

    Update App.jsx

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import OrdersPage from "./pages/OrderPage";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/users" element={<UsersPage /> }/>
           <Route path="/orders" element={<OrdersPage />} />
         </Routes>
 
